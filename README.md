da1de296353c76fd4cfee443a97feb32b0ae53cd
Jason Yarrington <jason@ffinno.com>
2014-06-26 19:52:02 -0400

    Added stuff

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ openhack-socket
 ===============
 
 OpenHack Socket Sandbox
+
+Installed on the dev server http://d.ffinno.com:8000/
+
--- server.js
+++ server.js
@@ -1,5 +1,14 @@
+
+
 var app=require("./app");
 var http=require("http");
-http.createServer(app).listen(8000, function(){
+
+var server = http.createServer(app).listen(8000, function(){
     console.log("Express listening on port 8000");
+});
+
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket){
+    console.log('a user connected');
 });
